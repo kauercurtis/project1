92c9b9d3a677adc87887b20b0b4a91bd81001592
Curtis Kauer <kauercurtis@gmail.com>
2025-10-12 11:58:22 -0700

    horse rendered

--- sample.cpp
+++ sample.cpp
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <ctype.h>
 #include <time.h>
-
+#include "CarouselHorse0.10.550"
 
 #ifndef F_PI
 #define F_PI		((float)(M_PI))
@@ -186,7 +186,8 @@ const int MS_PER_CYCLE = 10000;		// 10000 milliseconds = 10 seconds
 int		ActiveButton;			// current button that is down
 GLuint	AxesList;				// list to hold the axes
 int		AxesOn;					// != 0 means to draw the axes
-GLuint	BoxList;				// object display list
+GLuint	BoxList;
+GLuint  SolidHorseList;			
 int		DebugOn;				// != 0 means to print debugging info
 int		DepthCueOn;				// != 0 means to use intensity depth cueing
 int		DepthBufferOn;			// != 0 means to use the z-buffer
@@ -794,8 +795,7 @@ InitLists( )
 	float dz = 2.;
 	glutSetWindow( MainWindow );
 
-	// create the object:
-
+	// circle
 	BoxList = glGenLists( 1 );
 	glNewList( BoxList, GL_COMPILE );
 		float numsegs = 50.;
@@ -821,6 +821,41 @@ InitLists( )
 				ang += dang;
 			}
 		glEnd();
+		
+	SolidHorseList = glGenLists( 1 );
+	glNewList( SolidHorseList, GL_COMPILE );
+		glPushMatrix( );
+			glRotatef(90.f, 0., 1., 0.);
+			glTranslatef( 0., -1.1f, 0.f);
+			glBegin( GL_TRIANGLES );
+				for( int i = 0; i < HORSEntris; i++ )
+				{
+					struct point p0 = HORSEpoints[ HORSEtris[i].p0 ];
+					struct point p1 = HORSEpoints[ HORSEtris[i].p1 ];
+					struct point p2 = HORSEpoints[ HORSEtris[i].p2 ];
+
+					// fake "lighting" from above:
+
+					float p01[3], p02[3], n[3];
+					p01[0] = p1.x - p0.x;
+					p01[1] = p1.y - p0.y;
+					p01[2] = p1.z - p0.z;
+					p02[0] = p2.x - p0.x;
+					p02[1] = p2.y - p0.y;
+					p02[2] = p2.z - p0.z;
+					Cross( p01, p02, n );
+					Unit( n, n );
+					n[1] = (float)fabs( n[1] );
+					// simulating a glColor3f( 1., 1., 0. ) = yellow:
+					glColor3f( 1.f*n[1], 1.f*n[1], 0.f*n[1]);
+
+					glVertex3f( p0.x, p0.y, p0.z );
+					glVertex3f( p1.x, p1.y, p1.z );
+					glVertex3f( p2.x, p2.y, p2.z );
+				}
+			glEnd( );
+		glPopMatrix( );
+	glEndList( );	
 
 #ifdef NOTDEF
 		glColor3f(1., 1., 1.);
